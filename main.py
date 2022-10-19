4e4ebaf402683bc9ce07304ffeb6eea2b1df6535
haruya3 <genga711@gmail.com>
2022-10-19 15:32:09 +0900

    complete 'improve option'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from select import select
 from my_google.auth import Auth
 from my_google.my_drive.module import *
 from googleapiclient.errors import HttpError as HttpError
@@ -6,13 +7,14 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-def main(date):
+def main(date, download_flag, delete_flag):
     #グーグルサービスクライアント初期化
     SCOPES = [os.getenv('SCOPE')]
     GOOGLE_SECRET_PATH = os.getenv('GOOGLE_SECRET_PATH')
     drive = Auth(SCOPES, GOOGLE_SECRET_PATH, 'drive', 'v3')
 
-    download_ditection_data(drive, str(date))
+    if(download_flag):
+        download_ditection_data(drive, str(date))
 
 def download_ditection_data(drive, date):
     condition_list = [
@@ -36,10 +38,24 @@ def download_ditection_data(drive, date):
 def set_args():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w","--when", help="いつのデータを取得するか指定する", type=int)
+    parser.add_argument("-w", "--when", help="いつのデータを取得するか指定する。形式: (yyyymmdd)", type=int, required=True)
+    parser.add_argument("-d", "--download", help="ダウンロード", action='store_true')
+    parser.add_argument("-D", "--delete", help="いつのデータを取得するか指定する", action='store_true')
     return parser.parse_args()
+
+def check_google_drive_action(*actions):
+    selected_actions = list(filter(lambda x: x, actions))
+    if len(selected_actions) == 0:
+        print('Google Dirveへの操作オプションの指定は必須です。')
+        exit()
+
+    if selected_actions.count(True) >= 2:
+        print('Google Dirveへの操作オプションの指定は一つしかできません。')
+        exit()
 
 #TODO: Jins memeは眼鏡の置き方をミスるとデータ取得されちゃうので欲しい時間までを指定できるようにした方が良いかも
 if __name__ == '__main__':
     args = set_args()
-    main(args.when)
+    check_google_drive_action(args.download, args.delete)
+
+    main(args.when, args.download, args.delete)
